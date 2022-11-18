e1905be5dd9baad4345696de12169c22c42d714b
Shlomi <vgtvgy1@gmail.com>
2022-11-18 13:11:51 +0200

    Draw some pixels

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,11 @@ mod rom_parser;
 use log::info;
 use memory::{ROM, Memory, MemoryBus};
 use cpu::cpu::CPU;
+use render::sdl2_setup;
 use simple_logger::SimpleLogger;
 use rom_parser::RomParser;
 
-fn main() {
-	SimpleLogger::new().init().unwrap();
-
-	//let path = "6502asm_programs/nestest.nes";
-	//let path = "6502asm_programs/minimal.nes";
+fn greenscreen() {
 	let path = "6502asm_programs/greenscreen.nes";
 	let mut rom_parser = RomParser::new();
 	rom_parser.parse(path);
@@ -29,10 +26,33 @@ fn main() {
 	let memory_bus = MemoryBus::new(memory, rom);
 	let mut cpu = CPU::new(memory_bus);
 
-	let mut i = 1;
-	loop {
+	sdl2_setup();
+
+	for i in 0..20 {
 		info!("Assembly line: {}", i);
 		cpu.clock_tick();
-		i += 1;
+	}
+}
+
+fn main() {
+	SimpleLogger::new().init().unwrap();
+
+	greenscreen();
+	
+	let path = "C:\\Users\\Shlomi\\Desktop\\Projects\\nes-test-roms\\blargg_nes_cpu_test5\\cpu.nes";
+	let mut rom_parser = RomParser::new();
+	rom_parser.parse(path);
+	let prg_rom = rom_parser.prg_rom;
+	
+	let rom: ROM = ROM {
+		rom: prg_rom
 	};
+
+	let memory: Memory = [0; 32768];
+	let memory_bus = MemoryBus::new(memory, rom);
+	let mut cpu = CPU::new(memory_bus);
+
+	loop {
+		cpu.clock_tick();
+	}
 }
--- src/render.rs
+++ src/render.rs
@@ -2,7 +2,11 @@ extern crate sdl2;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::rect::{Rect};
 use std::time::Duration;
+
+const WIDTH: u32 = 10;
+const HEIGHT: u32 = 10;
 
 pub fn sdl2_setup() {
 	let sdl_context = sdl2::init().unwrap();
@@ -10,6 +14,7 @@ pub fn sdl2_setup() {
  
     let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
         .position_centered()
+		.resizable()
         .build()
         .unwrap();
  
@@ -20,20 +25,40 @@ pub fn sdl2_setup() {
     canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
     let mut i = 0;
+
+	let (mut win_width, mut win_height) = canvas.window_mut().size();
+
     'running: loop {
         i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
+
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running
                 },
+				Event::Window {..} => {
+					(win_width, win_height) = canvas.window_mut().size();
+					println!("Window size changed");
+				}
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
+
+		let pixel_size_width: u32 = win_width / WIDTH;
+		let pixel_size_height: u32 = win_height / HEIGHT;
+		for y in 0..HEIGHT {
+			for x in 0..WIDTH {
+				let pixel_x = x * pixel_size_width;
+				let pixel_y = y * pixel_size_height;
+				let rect = Rect::new(pixel_x as i32, pixel_y as i32, pixel_size_width as u32, pixel_size_height as u32);
+
+				canvas.set_draw_color(Color::RGB(200, 0, 0));
+				canvas.fill_rect(rect).unwrap();
+				canvas.set_draw_color(Color::RGB(0, 0, 200));
+				canvas.draw_rect(rect).unwrap();
+			}
+		}
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
